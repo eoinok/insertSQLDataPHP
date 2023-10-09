9b5372cb24d3dbde2e2bb22237396ff2e0d97b2a
eoinok <55285386+eoinok@users.noreply.github.com>
2023-10-09 23:05:26 +0100

    Update insertData.php

--- insertData.php
+++ insertData.php
@@ -8,9 +8,9 @@ $password = "pass";
 $dbname = "database";
 $port = 3307;
 
-
+//set the default timezone - this is necessary for PHP8
 date_default_timezone_set('Europe/Dublin');
-//date.timezone = Europe/Dublin;
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 // Check connection
